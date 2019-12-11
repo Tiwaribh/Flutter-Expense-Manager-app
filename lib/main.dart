3765988a009374ec5dd869a1a14a55ae08122f58
bhoopesh <bhoopesh.tiwari@ideavate.com>
2019-12-11 10:53:17 +0530

    Added switch to show chart or list

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           );
         });
   }
-
+  bool _showChart=false;
   @override
   Widget build(BuildContext context) {
 
@@ -113,12 +113,25 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+                Text("Show Chart"),
+                Switch(value: _showChart, onChanged: (val){
+                setState(() {
+                  _showChart=val;
+                });
+              },)
+            ],),
+
+           _showChart? Container(
                 height: (MediaQuery.of(context).size.height-appBar.preferredSize.height
                 - MediaQuery.of(context).padding.top
                 ) * 0.3,
-                child: Chart(_recentTransactions)),
-            Container(
+                child: Chart(_recentTransactions))
+           : Container(
                 height: (MediaQuery.of(context).size.height-appBar.preferredSize.height
                     - MediaQuery.of(context).padding.top
                 ) * 0.7,
